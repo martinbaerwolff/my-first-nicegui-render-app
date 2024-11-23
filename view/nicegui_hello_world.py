cdb9351a175219bcee880a3a39862a8dadab2523
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2024-11-23 22:17:19 +0100

    Warn user on reload

--- view/nicegui_hello_world.py
+++ view/nicegui_hello_world.py
@@ -4,10 +4,24 @@ from nicegui import app, ui
 from .map import map
 
 
+def enable_reload_warning():
+    if __debug__:
+        return
+    ui.run_javascript(
+        """
+            window.onbeforeunload = function(event) {
+                event.preventDefault();
+                event.returnValue = "Sind Sie sicher, dass Sie die Seite verlassen möchten? Änderungen werden verloren gehen.";
+                return "Sind Sie sicher, dass Sie die Seite verlassen möchten?";
+            };
+        """
+    )
+
+
 def init(fastapi_app: FastAPI) -> None:
     @ui.page('/')
     def show():
-        ui.page_title('Test NiceGUI + Render')
+        enable_reload_warning()
         ui.label('Hello, FastAPI!')
         ui.dark_mode().bind_value(app.storage.user, 'dark_mode')
         ui.checkbox('dark mode').bind_value(app.storage.user, 'dark_mode')
@@ -16,5 +30,6 @@ def init(fastapi_app: FastAPI) -> None:
     ui.run_with(
         fastapi_app,
         storage_secret=secrets.token_hex(32),
+        title="Test NiceGUI + Render",
         favicon="🐿️"
     )
